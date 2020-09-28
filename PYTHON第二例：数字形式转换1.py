d3800962b7ad9ef923140c1243167b8941fc5b75
deeplearnerleo <44855788+deeplearnerleo@users.noreply.github.com>
2020-09-28 13:25:23 +0800

    Update PYTHON第二例：数字形式转换1.py

--- PYTHON第二例：数字形式转换1.py
+++ PYTHON第二例：数字形式转换1.py
@@ -3,3 +3,11 @@ template = "零一二三四五六七八九"
 s = input()
 for c in s:
     print(template[eval(c)],end="")
+
+    
+'''
+end=""  print()中增加end=""参数表示输出后不增加换行，
+多个print()可以连续输出
+'''
+#问题1，template[]为什么可以表示为汉字字符串
+#问题2，template[]的汉字顺序是怎么确定的
